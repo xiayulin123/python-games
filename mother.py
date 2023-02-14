225b4a7b9944f445cc53bf308dfedef520b1f28d
xiayulin123 <113715923+xiayulin123@users.noreply.github.com>
2023-02-13 21:38:07 -0500

    Add files via upload

---
+++ mother.py
@@ -0,0 +1,7 @@
+from PIL import Image
+#Imports PIL module
+# open method used to open different extension image file
+im = Image.open(r'C:\Users\xiayu\PycharmProjects\pythonProject\school hw\game\alien-invasion\images\zp.jpg')
+
+# This method will show image in any image viewer
+im.show()
